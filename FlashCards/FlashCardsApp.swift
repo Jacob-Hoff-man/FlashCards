ed113b70623f86c01f5dfc84d9079ceff12d1f5b
Jacob Hoffman <cubhoffman@gmail.com>
2020-12-30 10:18:22 -0500

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Jacob Hoffman on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
